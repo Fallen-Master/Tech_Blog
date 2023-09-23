4568cd9aa9dc239b5bdb00eccc0bae924a748451
Rafael Gomez <rgomez2014@gmail.com>
2023-09-23 18:14:16 -0500

    created Post model

---
+++ models/post.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes, INTEGER } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        user_id: {
+          type: DataTypes.INTEGER,
+          references: {
+              model: 'user',
+              key: 'id',
+          }
+      }
+    },
+    {
+        hooks: {
+          beforeCreate: async (newPostData) => {
+            newPostData.content = await (newPostData.content);
+            return newPostData;
+          },
+          beforeUpdate: async (updatedPostData) => {
+            updatedPostData.content = await (updatedPostData.content );
+            return updatedPostData;
+          },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+      }
+);
+
+module.exports = Post;
