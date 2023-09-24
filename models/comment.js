14babc2a2312c1dfdb770081f5eb981be0da0164
Rafael Gomez <rgomez2014@gmail.com>
2023-09-23 19:00:21 -0500

    created Comment model

---
+++ models/comment.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes, INTEGER } = require('sequelize');
+const sequelize = require('../config/connection');
+const { text } = require('express');
+
+class Comment extends Model { }
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        dateCreated: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+            allowNull: false,
+        },
+        postId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id',
+                onDelete: 'CASCADE'
+            }
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+                onDelete: 'CASCADE'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
+
+module.exports = Comment;
