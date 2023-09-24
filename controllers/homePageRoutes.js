d59d889011a0b00b6672b8d34091514926add2c1
Rafael Gomez <rgomez2014@gmail.com>
2023-09-23 23:25:01 -0500

    setup homePageRoutes

--- controllers/homePageRoutes.js
+++ controllers/homePageRoutes.js
@@ -1,7 +1,67 @@
 const router = require('express').Router();
+const { Post, User } = require('../models')
+const withAuth = require('../utils/auth')
 
 router.get('/', async (req, res) => {
-  res.render('homepage');
+  try {
+    const postData = await Post.findAll();
+    const post = postData.map((pos) => pos.get({ plain: true }))
+    res.render('homePage', {
+      post,
+      logged_in: req.session.logged_in
+    })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.get('/post/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          attributes: ['content']
+        }
+      ]
+    });
+    const post = postData.get({ plain: true });
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.get('./dashboard', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: ['username'],
+      include: [
+        {
+          model: Post,
+          attributes: ['title', 'content', 'dateCreated']
+        },
+      ],
+    });
+    const userPost = userData.Post.map((post) => post.get({ plain: true }));
+    res.render('dashboard', {
+      userPost,
+      logged_in: req.session.logged_in
+    })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('login');
 });
 
 module.exports = router;
