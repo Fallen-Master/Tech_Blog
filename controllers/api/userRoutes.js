0216cc5948ef91057daf63520f763bc566d6164f
Rafael Gomez <rgomez2014@gmail.com>
2023-09-30 21:40:27 -0500

    this works now too

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.get('/signup', (req, res) => {
+  res.render('signup') 
+  return; 
+});
+
+
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create({
